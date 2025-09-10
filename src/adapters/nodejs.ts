d2e4e65e9d4a935a83cf286d7f0cce0d039c1d1f
jinzhongjia <mail@nvimer.org>
2025-09-10 19:31:50 +0800

    fix(adapters): 修复 NodeJS 图像适配器的图像大小处理
    - 更新 image-size 模块导入方式，使用解构导入
    - 移除 imageSize.disableTypes() 方法调用
    - 简化图像大小获取逻辑

--- src/adapters/nodejs.ts
+++ src/adapters/nodejs.ts
@@ -1,4 +1,4 @@
-import * as imageSize from 'image-size'
+import { imageSize } from 'image-size'
 import { Tokens } from 'marked'
 import fs from 'node:fs/promises'
 import http from 'node:http'
@@ -16,7 +16,7 @@ export const downloadImage: MarkdownImageAdapter = async function (token: Tokens
   try {
     const buffer = await loadImage(src)
 
-    const { width, height, type } = imageSize.default(buffer)
+    const { width, height, type } = imageSize(buffer)
 
     const supportType = getImageExtension(src, type)
 
@@ -53,7 +53,6 @@ function loadImage (src: string) {
         })
         res.on('end', () => {
           const buffer = Buffer.concat(chunks)
-          imageSize.disableTypes(['svg', 'webp'])
           resolve(buffer)
         })
         res.on('error', (err) => {
